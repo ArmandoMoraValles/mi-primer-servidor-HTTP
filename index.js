105057dba56e9852a38538cf896d5923a02fdd08
armandomoravalles <armandomoravalles@gmail.com>
2023-11-21 21:29:40 -0600

    3) Servidor síncrono que lea y envíe un archivo.

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 const http = require('http');
-const PORT = 3000;
+const fs = require('fs');
+
+const port = 3000;
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
+  // Leer la ubicacion del archivo desde el url
+  const url = req.url;
 
-  res.end('Hola, Mundo!');
+  try {
+    const fileContent = fs.readFileSync(`.${url}`, 'utf8');
+
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+
+    res.end(fileContent);
+  } catch (error) {
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('Archivo no encontrado');
+  }
 });
 
-server.listen(PORT, () => {
-  console.log(`EL SERVIDOR SE ESTA EJECUTANDO EN EL PUERTO http://localhost:${PORT}/`);
+server.listen(port, () => {
+  console.log(`EL SERVIDOR SE ESTA EJECUTANDO EN EL PUERTO http://localhost:${port}/`);
 });
