536f6a960cf4c8de43212074351b5b634cf573d3
armandomoravalles <armandomoravalles@gmail.com>
2023-11-21 21:16:25 -0600

    2) Servidor simple de hola mundo en node.js usando HTTP.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('Hola, Mundo!');
+const http = require('http');
+const PORT = 3000;
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+
+  res.end('Hola, Mundo!');
+});
+
+server.listen(PORT, () => {
+  console.log(`EL SERVIDOR SE ESTA EJECUTANDO EN EL PUERTO http://localhost:${PORT}/`);
+});
